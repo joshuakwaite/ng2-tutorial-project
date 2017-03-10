4713f46fa2c4d8217d766cf12fc96541102aba22
Josh Waite <kalewaite@gmail.com>
2017-03-10 10:16:52 -0700

    Working on last part

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { HeroDetailComponent } from './hero-detail-component/hero-detail.component';
+import { HeroesComponent } from './heroes.component';
+import { HeroService } from './hero.service.ts/hero.service';
+import { DashboardComponent } from './dashboard.component/dashboard.component';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroDetailComponent,
+    HeroesComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [HeroService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
